e040bd0dad3fea399f7abafd895a45a6f32a88a0
danyahh429 <danyah.harris3@gmail.com>
2018-11-16 16:40:20 -0800

    Update project2.s

--- project2.s
+++ project2.s
@@ -3,8 +3,13 @@
     lengthErrorMessage: .asciiz "Input is too long."
     baseErrorMessage:   .asciiz "Invalid base-34 number."
     userInput:		.space 50
- .text
+.text
     main:
 	li $v0, 8       #get user input as txt 
 	la $a0, userInput
 	li $a1, 50
+li $a1, 50
+	syscall
+	
+	removeLeading:  #Remove leading spaces
+	li $t8, 32      #Save space char to t8
