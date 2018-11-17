45007f6ea9c19dcb38e43dad5ce89a1b3ecac96f
danyahh429 <danyah.harris3@gmail.com>
2018-11-16 16:48:58 -0800

    Update project2.s

--- project2.s
+++ project2.s
@@ -28,3 +28,8 @@
 
 	lengthLoop:
 	lb $t2, 0($a0)   #Load the next char to t2
+	beqz $t2, done   #End loop if null char is reached
+	beq $t2, $t1, done   #End loop if end-of-line is detected
+	addi $a0, $a0, 1   #Increment the string ptr
+	addi $t0, $t0, 1
+	j lengthLoop
