d08ce78b6f20f3e3bb24a1143ee0e161082522bc
danyahh429 <danyah.harris3@gmail.com>
2018-11-16 16:45:39 -0800

    Update project2.s

--- project2.s
+++ project2.s
@@ -5,16 +5,21 @@
     userInput:		.space 50
 .text
     main:
-li $v0, 8       #get user input as txt 
-la $a0, userInput
-li $a1, 50
-li $a1, 50
-syscall
+	li $v0, 8       #get user input as txt 
+	la $a0, userInput
+	li $a1, 50
+	li $a1, 50
+	syscall
 	
-removeLeading:  #Remove leading spaces
-li $t8, 32      #Save space char to t8
-beq $t8, $t9, removeFirst
-move $t9, $a0
-j checkLength
+	removeLeading:  #Remove leading spaces
+	li $t8, 32      #Save space char to t8
+	beq $t8, $t9, removeFirst
+	move $t9, $a0
+	j checkLength
 
-removeFirst:
+	removeFirst:
+	addi $a0, $a0, 1
+	j removeLeading
+
+	checkLength:   #Count the number of chars in string
+	addi $t0, $t0, 0  #Initialize count at zero
