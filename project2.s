7edcd2faecd874a84774a7af9c144f18d43f501e
danyahh429 <danyah.harris3@gmail.com>
2018-11-16 16:37:18 -0800

    Create  project2.s

---
+++ project2.s
@@ -0,0 +1,6 @@
+.data
+    nullErrorMessage:	.asciiz "Input is empty."
+    lengthErrorMessage: .asciiz "Input is too long."
+    baseErrorMessage:   .asciiz "Invalid base-34 number."
+    userInput:		.space 50
+  
