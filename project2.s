bfb7ae7a2bf62358e016d8d737c1dd21facc639a
danyahh429 <danyah.harris3@gmail.com>
2018-11-16 16:46:39 -0800

    Update project2.s

--- project2.s
+++ project2.s
@@ -23,3 +23,8 @@
 
 	checkLength:   #Count the number of chars in string
 	addi $t0, $t0, 0  #Initialize count at zero
+	addi $t1, $t1, 10  #Save line feed charr to t1
+	add $t4, $t4, $a0  #Preserve the content of a0
+
+	lengthLoop:
+	lb $t2, 0($a0)   #Load the next char to t2
